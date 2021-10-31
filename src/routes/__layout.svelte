d4ac30791c4c706f36c906f30ee1a9bf059c557a
a-bishop <andrew.bishop53@gmail.com>
2021-10-30 22:43:05 -0700

    better mobile styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,23 +4,23 @@
 </script>
 
 <style>
-	footer {
+	/* footer {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		padding: 40px;
-	}
+	} */
 
 	/* footer a {
 		font-weight: bold;
 	} */
 
-	@media (min-width: 480px) {
+	/* @media (min-width: 480px) {
 		footer {
 			padding: 40px 0;
 		}
-	}
+	} */
 </style>
 
 <main>
